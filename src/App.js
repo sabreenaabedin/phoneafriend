b604bf11890936003313e9fa0e8a9a97b812230b
Sabreena <sabreenaabedin@gmail.com>
2020-06-07 20:15:25 -0400

    connect to algolia

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
-import './App.css';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('GZ8KQTAT6T', '703ac21cdbe53afa5221672363c84cfb');
+
+const Hit = ({hit}) =>
+  <div className="hit">
+      <div className = "hitName">
+        {hit.name}
+      </div>
+  </div>
+
+const Content = () =>
+  <div className = "content">
+    <Hits hitComponent = {Hit}/>
+  </div>
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-    );
+    return(
+      <InstantSearch searchClient={searchClient} indexName="test_phoneafriend_query_suggestions">
+        <header>
+          <SearchBox translations = {{placeholder:'Search'}}/>
+        </header>
+        <main>
+          <Content/>
+        </main>
+      </InstantSearch>
+    )
   }
-}
+};
 
 export default App;
